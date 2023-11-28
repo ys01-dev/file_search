81cf729310aa94923fecf5c0fa8363e2719d8ef8
ys01-dev <matuyanotenpura@gmail.com>
2023-11-29 04:24:26 +0900

    Add project files.

---
+++ WindowsProject1/resource.h
@@ -0,0 +1,37 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by WindowsProject1.rc
+//
+#define IDC_MYICON                      2
+#define IDS_APP_TITLE                   101
+#define IDR_MAINFRAME                   123
+#define IDR_ACCELERATOR1                132
+#define IDM_ABOUT                       201
+#define IDM_SEARCH                      202
+#define IDM_EXIT                        203
+#define IDD_WINDOWSPROJECT1_DIALOG      301
+#define IDD_ABOUTBOX                    302
+#define IDD_SEARCHBOX                   303
+#define IDI_WINDOWSPROJECT1             401
+#define IDI_SMALL                       402
+#define ID_SEARCH                       501
+#define ID_ACCEL_SELECTALL              601
+#define ID_ACCEL_SAVE                   602
+#define IDC_WINDOWSPROJECT1             1001
+#define IDC_TEXTBOX                     1002
+#define IDC_PROGRESSBAR                 1003
+#define IDC_SYSLINK1                    1004
+#define IDC_TEXT1                       1005
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        133
+#define _APS_NEXT_COMMAND_VALUE         32774
+#define _APS_NEXT_CONTROL_VALUE         1006
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
